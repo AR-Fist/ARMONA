263e0a5ff4228a7276f2c7329fb548e743106f17
ThammakornK <chan_tham@hotmail.com>
2021-01-06 19:40:59 +0700

    Re-create

--- app/src/androidTest/java/com/arfist/armona/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/arfist/armona/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.armona
+package com.arfist.armona
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.armona", appContext.packageName)
+        assertEquals("com.arfist.armona", appContext.packageName)
     }
 }
--- app/src/test/java/com/arfist/armona/ExampleUnitTest.kt
+++ app/src/test/java/com/arfist/armona/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.armona
+package com.arfist.armona
 
 import org.junit.Test
 
