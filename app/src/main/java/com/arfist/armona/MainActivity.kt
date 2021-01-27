ea73937b7d918f76854e8a8a00afaf8cf03fd5b7
sprkzoff <sprkzoff@gmail.com>
2021-01-27 16:21:18 +0700

    Integrated OpenCV

--- app/src/main/java/com/arfist/armona/MainActivity.kt
+++ app/src/main/java/com/arfist/armona/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.arfist.armona
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(OpenCVLoader.initDebug()) {
+            Log.i("OpenCV","init successful");
+        }
+        else {
+            Log.i("OpenCV","init fail");
+        }
     }
 }
