61c22fc55c68c5cb514e4aaa24bb252e2a5c97d9
sprkzoff <sprkzoff@gmail.com>
2021-01-31 00:06:01 +0700

    Import OpenCV 3.4.10

--- app/src/main/java/com/arfist/armona/MainActivity.kt
+++ app/src/main/java/com/arfist/armona/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.arfist.armona
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(OpenCVLoader.initDebug()) {
+            Log.d(TAG,"Load OpenCV successful");
+        }
+        else {
+            Log.d(TAG,"Load OpenCV fail");
+        }
     }
 }
