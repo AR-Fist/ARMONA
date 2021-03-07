7f919d1bc970124318b8d90c017326dd1325d8c8
Tanakorn Pisnupoomi <intaniger@gmail.com>
2021-03-08 05:19:33 +0700

    Integrated AR with camera module

--- app/src/main/java/com/arfist/armona/screen/ar/ArFragment.kt
+++ app/src/main/java/com/arfist/armona/screen/ar/ArFragment.kt
@@ -1,12 +1,15 @@
 package com.arfist.armona.screen.ar
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
@@ -14,12 +17,29 @@ import com.arfist.armona.R
 import com.arfist.armona.databinding.ArFragmentBinding
 import com.arfist.armona.screen.map.MapViewModel
 import timber.log.Timber
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+
 
 class ArFragment : Fragment() {
-
     private lateinit var viewModel: ArViewModel
     private val mapViewModel: MapViewModel by activityViewModels()
     private lateinit var binding: ArFragmentBinding
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private fun allRequiredPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +47,35 @@ class ArFragment : Fragment() {
     ): View {
         Timber.i("onCreateView")
 
+        if (this.allRequiredPermissionsGranted()){
+            setupCam(container)
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
         binding = DataBindingUtil.inflate(inflater, R.layout.ar_fragment, container, false)
         return binding.root
+    }
+
+    private fun setupCam(container: ViewGroup?) {
+        val cameraProvider = requireContext().let { ProcessCameraProvider.getInstance(it) }
+        cameraProvider.addListener({
+            bindPreview(cameraProvider.get(), container)
+        }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun bindPreview(cameraProvider : ProcessCameraProvider, container: ViewGroup?) {
+        val preview : Preview = Preview.Builder()
+            .build()
+
+        val cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val previewView: PreviewView = container!!.findViewById<PreviewView>(R.id.previewView)
+        previewView.preferredImplementationMode = PreviewView.ImplementationMode.SURFACE_VIEW
+        preview.setSurfaceProvider(previewView.createSurfaceProvider())
+
+        cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
